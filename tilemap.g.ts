2ecbb47f65790d7c4539e57e4e645b73dac28bf3
danyboy567 <123103092+danyboy567@users.noreply.github.com>
2023-03-23 08:11:42 -0700

    Update main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,6 +8,8 @@ namespace myTiles {
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -297,7 +299,7 @@ namespace myTiles {
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,sprites.dungeon.floorDark0,sprites.dungeon.floorMixed,myTiles.tile1,myTiles.tile2,myTiles.tile3,sprites.dungeon.doorClosedSouth,sprites.dungeon.floorDark4], TileScale.Sixteen);
             case "level15":
-            case "level15":return tiles.createTilemap(hex`10000e00151515151517171716161616161616161515151515020b01010b0316161616161515151515051213121104161616161615151515150512121213041616161616151515151505141414140416161616161502010b01071213121206010b01031615051312111213121212121312120416150d1212121212131213121212120c161505121312131212121211121213041615090a0e0a0a1019190f0a0a0e0a081615151515151505131204161616161616151515151515051a1a041616161616161515151515150918180816161616161615151515151515151616161616161616`, img`
+            case "level15":return tiles.createTilemap(hex`10000e00151515151517171716161616161616161515151515020b01010b03161616161615151515150512131211041616161616151515151505121212130416161616161515151515051b1414140416161616161502010b01071213121206010b01031615051312111213121212121312120416150d1212121212131213121212120c161505121312131212121211121213041615090a0e0a0a1012130f0a0a0e0a081615151515151505191904161616161616151515151515051a1a041616161616161515151515150918180816161616161615151515151515151616161616161616`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . 2 2 2 2 2 2 . . . . . 
 . . . . . 2 . . . . 2 . . . . . 
@@ -312,7 +314,7 @@ namespace myTiles {
 . . . . . . 2 . . 2 . . . . . . 
 . . . . . . 2 2 2 2 . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.floorDark3,sprites.dungeon.floorDark0,sprites.dungeon.floorDark1,sprites.dungeon.stairLadder,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.floorDark4,sprites.dungeon.floorMixed], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.floorDark3,sprites.dungeon.floorDark0,sprites.dungeon.floorDark1,sprites.dungeon.stairLadder,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.floorDark4,sprites.dungeon.floorMixed,myTiles.tile4], TileScale.Sixteen);
         }
         return null;
     })
@@ -326,6 +328,8 @@ namespace myTiles {
             case "tile2":return tile2;
             case "myTile1":
             case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
         }
         return null;
     })
